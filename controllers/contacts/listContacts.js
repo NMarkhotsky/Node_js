72d8a6a58508a0a66f63f2dc4aa734b0ee4d8dcf
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-06-28 12:24:00 +0300

    added filtering contacts by field favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -3,12 +3,21 @@ const { ctrlWrapper } = require('../../utils');
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 5, favorite } = req.query;
+
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, '', { skip, limit });
+  const searchQuery = { owner };
+  if (favorite === 'true') searchQuery.favorite = true;
+  if (favorite === 'false') searchQuery.favorite = false;
 
-  res.json(result);
+  const result = await Contact.find(searchQuery, '', { skip, limit });
+  const total = await Contact.count(searchQuery);
+
+  res.status(200).json({
+    result,
+    total,
+  });
 };
 
 module.exports = ctrlWrapper(listContacts);
